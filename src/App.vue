2bf4b243dc017fb30907fe9a67b80777056b8651
Mo Essam <mohameddessam303@gmail.com>
2023-10-11 18:30:01 +0300

    container

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,9 @@ import Footer from './components/Footer.vue'
 </script>
 
 <script>
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 export default {
   components:{
     Navbar,
@@ -22,7 +25,37 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.our-container {
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+/* Small */
+
+@media (min-width: 768px) {
+  .our-container {
+    width: 750px;
+  }
+}
+
+/* Medium */
+
+@media (min-width: 992px) {
+  .our-container {
+    width: 970px;
+  }
+}
+
+/* Large */
+
+@media (min-width: 1200px) {
+  .our-container {
+    width: 1170px;
+  }
+}
 main{
   min-height: 100vh;
 }
