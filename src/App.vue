87bc032df43ac9558b50ade4b9a0fee46211efcd
al1maher <aly.godfather.gm@gmail.com>
2023-10-11 16:15:05 +0300

    First Init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    <i class="fa-solid fa-brain"></i>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import '@fortawesome/fontawesome-svg-core'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
