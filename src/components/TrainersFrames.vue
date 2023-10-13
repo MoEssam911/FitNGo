0e5dcc7d27275970c0cad3c0be95f1eb59b4e78f
Hassan7733 <hsngaml@gmail.com>
2023-10-13 22:32:00 +0300

    gym location section and finished trainer frames

--- src/components/TrainersFrames.vue
+++ src/components/TrainersFrames.vue
@@ -1,87 +1,99 @@
 <template>
-    <section class="container mx-auto">
-        <h2 class="text-2xl text-primary font-bold ml-3 my-5">Trainers</h2>
-        <div class="flex gap-1">
-            <div class="overflow-hidden transition-width frame w-2/12 hover:w-5/12 duration-500 rounded-xl relative text-white text-center leading-[612px] hover:text-left hover:leading-none pt-5 pl-5"  v-for="da in data" :key="da">
-                <img :src="da.trainerImg" alt="" class="h-full  object-cover absolute left-0 top-0">
-                <h2 >{{ da.trainerNames }}</h2>
-                <div v-html="da.rate" class="rate mt-40 text-primary"></div>
-                <div id="trainer-data"><p class="w-10/12 mx-auto ">{{ da.Description }}</p>
-                <button class="border rounded-xl bg-primary text-white px-10 py-2 block mx-auto mt-6 transition duration-200">Info</button></div>
-            </div>
-            
-            <!-- {{ data }} -->
+  <section class="container mx-auto">
+    <h2 class="text-2xl text-primary font-bold ml-3 my-5">Trainers</h2>
+    <div class="flex gap-1">
+      <div
+        class="overflow-hidden transition-width frame w-2/12 hover:w-5/12 duration-1000 rounded-xl relative text-white text-center leading-[612px] hover:text-left hover:leading-none pt-5 pl-5"
+        v-for="da in data"
+        :key="da"
+      >
+        <img
+          :src="da.trainerImg"
+          alt=""
+          class="h-full object-cover absolute left-0 top-0"
+        />
+        <div class="trainer-data"> <h2>{{ da.trainerNames }}</h2>
+        <div v-html="da.rate" class="rate mt-40 text-primary"></div>
+        
+          <p class="w-10/12 mx-auto">{{ da.Description }}</p>
+          <button
+            class=" rounded-xl bg-primary text-white px-10 py-2 block mx-auto mt-6 transition duration-200"
+          >
+            Info
+          </button>
         </div>
-    </section>
+      </div>
+
+      <!-- {{ data }} -->
+    </div>
+  </section>
 </template>
 
 <script>
-import axios from 'axios'
-    export default {
-        name:"TrainersFrame",
-        data(){
-            return{
-                // trainerImg:[
-                //     '../src/assets/Images/Ahmed-Fathy.png',
-                //     '../src/assets/Images/aly.jpg',
-                //     '../src/assets/Images/gorilla.jpg',
-                //     '../src/assets/Images/7ossam.jpg',
-                //     '../src/assets/Images/momen.jpg',
-                //     '../src/assets/Images/omar.jpg',
-                // ],
-                // trainerNames:[
-                //     'C-Ahmed Fathy',
-                //     'C-Ali Mazhar',
-                //     'C-Hassan Gabr',
-                //     'C-Hossam',
-                //     'C-Moamen Maher',
-                //     'C-Omar Ibrahim',
-                // ]
-                data:[]
-            }
-        },
-        created(){
-            axios.get('http://localhost:3000/trainer').then((res)=>{
-                this.data = res.data;
-            }).catch(err=>console.log(err))
-        }
-        
-    }
+import axios from "axios";
+export default {
+  name: "TrainersFrame",
+  data() {
+    return {
+      // trainerImg:[
+      //     '../src/assets/Images/Ahmed-Fathy.png',
+      //     '../src/assets/Images/aly.jpg',
+      //     '../src/assets/Images/gorilla.jpg',
+      //     '../src/assets/Images/7ossam.jpg',
+      //     '../src/assets/Images/momen.jpg',
+      //     '../src/assets/Images/omar.jpg',
+      // ],
+      // trainerNames:[
+      //     'C-Ahmed Fathy',
+      //     'C-Ali Mazhar',
+      //     'C-Hassan Gabr',
+      //     'C-Hossam',
+      //     'C-Moamen Maher',
+      //     'C-Omar Ibrahim',
+      // ]
+      data: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/trainer")
+      .then((res) => {
+        this.data = res.data;
+      })
+      .catch((err) => console.log(err));
+  },
+};
 </script>
 
 <style scoped>
-
 .frame {
-    height: 600px;
-    background: white;
-    transition: .5s;
+  height: 600px;
+  background: white;
+  transition: 0.5s;  
+}
+.trainer-data p {
+  transition: 1s;
 }
 
-
-
-#trainer-data {
-    opacity: 0;
-    top: 150px;
-    transition: .8s;
-    transition-delay: 2s;
+.trainer-data {
+position: relative;
+transform: translateY(600px);
+  transition: 1s;
 }
-.frame:hover #trainer-data{
+.frame:hover .trainer-data {
+  z-index: 12;
+  position: relative;
+  transform: translateY(80px);
+}
+
+.frame h2 {
+opacity: 0;
+  transition: 0.5s;
+}
+.frame:hover h2 {
     opacity: 1;
-    z-index: 12;
-    position: relative;
-    transform: translateY(-50px);
-    transition-delay: 2s;
-    transition: .8s;
-}
-
-.frame h2{
-    z-index: 12;
-    transition: .5s;
-    transition-delay: .3s;
-}
-.frame:hover h2{
-    transform: translateX(-10px);
-    transform: translateY(10px);
+  transform: translateX(-10px);
+  transform: translateY(-80px);
 }
 /* .frame span,
 .frame p{
@@ -106,14 +118,14 @@ import axios from 'axios'
     width: 100px;
     height: 100px;
 } */
-.frame::before{
-    content: '';
-    width: 100%;
-    height: 100%;
-    background: #0000004a;
-    position: absolute;
-    left: 0;
-    top: 0;
-    z-index: 10;
+.frame::before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  background: #0000004a;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 10;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import OurPartners from "../components/OurPartners.vue"
 import TrainersFrame from "../components/TrainersFrames.vue"
+import GymLocations from "../components/GymLocations.vue"
 </script>
 
 <template>
   <div>
     <TrainersFrame></TrainersFrame>
+    <GymLocations></GymLocations>
     <OurPartners></OurPartners>
   </div>
 </template>
