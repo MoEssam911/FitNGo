ed5fb7d0bcbedbeca476d2a00e359b2432ad7581
Mo Essam <mohameddessam303@gmail.com>
2023-10-16 18:35:14 +0300

    create macro

---
+++ src/components/MacroCalculator.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <div class="container">
+      <h2>Macro Calculator</h2>
+      <form>
+        <label for="calories">Calories (per day)</label>
+        <input type="text" name="calories">
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
