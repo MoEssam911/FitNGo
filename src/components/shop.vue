5fa4b586a401355a454132228fcf9a73f33db7c7
“Nadaashrafrayan” <nada.rayan9416@gmail.com>
2023-10-18 16:53:42 +0300

    shopEdit

--- src/components/shop.vue
+++ src/components/shop.vue
@@ -14,7 +14,9 @@
 
     <!-- ad banners  -->
     <div class="flex relative object-cover">
-      <div class="w-1/2 h-1/4 transition ease-in-out  hover:-translate-y-1 hover:scale-110 duration-300 ">
+      <div
+        class="w-1/2 h-1/4 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+      >
         <img
           src="../../src/assets/Images/08f1c52293c3de74e6874e9dbc83aceb.png"
         />
@@ -22,10 +24,9 @@
       <div class="w-1/2 h-1/4">
         <img class="blur-[0.5px]" src="../../src/assets/Images/zegama.jpg" />
         <p
-          class="absolute left-[50%] inset-0 flex font-Manrope whitespace-pre-line p-[5%] text-primary text-5xl font-extrabold transition ease-in-out  hover:-translate-y-1 hover:scale-110 duration-300"
+          class="absolute left-[50%] inset-0 flex font-Manrope whitespace-pre-line p-[5%] text-primary text-5xl font-extrabold transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
         >
-          SALE 15% 
-          ON SPORT SHOES
+          SALE 15% ON SPORT SHOES
         </p>
       </div>
     </div>
@@ -156,7 +157,7 @@ export default {
   position: absolute;
   left: 16%;
   top: 5%;
-  transform: translate(-25%, -25%);
+  transform: translate(-90%, -90%);
   z-index: 1;
 }
 </style>
