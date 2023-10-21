e30669455d610a1228793c54580738772d782935
Elshimi <mid9653@gmail.com>
2023-10-21 13:40:07 -0700

    user side bar updated and we gonna compine all user

--- src/components/UserAccount/UserPlans.vue
+++ src/components/UserAccount/UserPlans.vue
@@ -1,5 +1,30 @@
 <template>
-    <div class="bg-secondary w-full mt-20 p-6">
+    <div class="container relative">
+        <div class="flex gap-2 mt-28 pb-6 items-center cursor-pointer">
+        <div class="md:hidden">
+          <MenuIcon @click="toggleUserSideHandler"></MenuIcon>
+        </div>
+        <h1 class="font-Manrope font-bold text-lg">My Account</h1>
+        <div>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 30 30" 
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M24.125 11.1562L18.8125 5.90625L20.5625 4.15625C21.0417 3.67708 21.6304 3.4375 22.3288 3.4375C23.0271 3.4375 23.6154 3.67708 24.0938 4.15625L25.8438 5.90625C26.3229 6.38542 26.5729 6.96375 26.5938 7.64125C26.6146 8.31875 26.3854 8.89667 25.9062 9.375L24.125 11.1562ZM22.3125 13L9.0625 26.25H3.75V20.9375L17 7.6875L22.3125 13Z"
+              fill="black" />
+          </svg>
+        </div>
+      </div>
+        <div class="flex gap-5">
+        <aside
+          class="w-6/12 sm:w-5/12 md:w-4/12 transition-all duration-1000"
+          :class="toggleUserSide ? 'show-user-sidebar' : 'hide-user-sidebar'">
+          <UserSideBar class="w-full"></UserSideBar>
+        </aside>
+    <div class="bg-secondary md:w-10/12 w-full  p-6 rounded-2xl">
         <div class="flex justify-between">
             <i class="fa-solid fa-caret-left text-4xl hover:cursor-pointer hover:text-primary inline-block" @click="change('left')" v-if="planTarget > 0"></i>
             <i class="fa-solid fa-caret-right text-4xl hover:cursor-pointer hover:text-primary inline-block ms-auto" @click="change('right')" v-if="planTarget < maxLength"></i>
@@ -46,12 +71,20 @@
                     </table>
         </div>
     </div>
+    </div>
+    </div>
 </template>
 
 <script>
+import UserSideBar from '../Tools/UserSideBar.vue'
+import MenuIcon from '../Tools/MenuIcon.vue'
 import axios from 'axios';
     export default {
         name: "UserPlans",
+        components:{
+            UserSideBar,
+            MenuIcon,
+        },
         created(){
             axios.get('http://localhost:3000/workoutplans').then((res)=>{
                 this.plans=res.data;
@@ -60,6 +93,7 @@ import axios from 'axios';
         },
         data(){
             return{
+                toggleUserSide: false,
                 planTarget : 0,
                 maxLength: 0,
                 plans:[
@@ -80,11 +114,26 @@ import axios from 'axios';
                     }
                     this.planTarget -= 1;
                 }
-            }
+            },
+            toggleUserSideHandler() {
+      this.toggleUserSide = !this.toggleUserSide;
+    },
         }
     }
 </script>
 
 <style scoped>
-
+@media (max-width: 768px) {
+  .hide-user-sidebar {
+    position: absolute;
+    left: -60%;
+    z-index: 10;
+  }
+  .show-user-sidebar {
+    position: absolute;
+    left: 0;
+    z-index: 10;
+    /* box-shadow: 1rem 0px 5px 0px black; */
+  }
+}
 </style>
