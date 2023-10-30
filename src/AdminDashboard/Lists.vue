3c870a3e26ede5886bd02b57ad98af3ad61499ca
“Nadaashrafrayan” <nada.rayan9416@gmail.com>
2023-10-30 05:01:07 +0200

    new edits

--- src/AdminDashboard/Lists.vue
+++ src/AdminDashboard/Lists.vue
@@ -1,48 +1,51 @@
 <template>
-    <div class="flex gap-32 justify-center mt-6">
-      <div
-        class="text-lg font-semibold hover:text-primary transition duration-200 cursor-pointer" @click="changetoggle('trainers')"
-        :class="{activeCol : toggle=='trainers'}"
-      >
-        Trainers
-      </div>
-      <div class="text-lg font-semibold">|</div>
-      <div
-        class="text-lg font-semibold hover:text-primary transition duration-200 cursor-pointer" @click="changetoggle('products')"
-        :class="{activeCol : toggle=='products'}"
-      >
-        Products
-      </div>
-      <div class="text-lg font-semibold">|</div>
-      <div
-        class="text-lg font-semibold hover:text-primary transition duration-200 cursor-pointer" @click="changetoggle('workouts')"
-        :class="{activeCol : toggle=='workouts'}"
-      >
-        Workouts
-      </div>
+  <div class="flex gap-32 justify-center mt-6">
+    <div
+      class="text-lg font-semibold hover:text-primary transition duration-200 cursor-pointer"
+      @click="changetoggle('trainers')"
+      :class="{ activeCol: toggle == 'trainers' }"
+    >
+      Trainers
     </div>
+    <div class="text-lg font-semibold">|</div>
+    <div
+      class="text-lg font-semibold hover:text-primary transition duration-200 cursor-pointer"
+      @click="changetoggle('products')"
+      :class="{ activeCol: toggle == 'products' }"
+    >
+      Products
+    </div>
+    <div class="text-lg font-semibold">|</div>
+    <div
+      class="text-lg font-semibold hover:text-primary transition duration-200 cursor-pointer"
+      @click="changetoggle('Users')"
+      :class="{ activeCol: toggle == 'Users' }"
+    >
+      Users
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'lists',
-        data(){
-            return {
-            toggle:'trainers'
-            }
-        },
-        inject:['changeView'],
-        methods:{
-            changetoggle(togg){
-                this.changeView(togg)
-               this.toggle = togg
-            }
-        }
-    }
+export default {
+  name: "lists",
+  data() {
+    return {
+      toggle: "trainers",
+    };
+  },
+  inject: ["changeView"],
+  methods: {
+    changetoggle(togg) {
+      this.changeView(togg);
+      this.toggle = togg;
+    },
+  },
+};
 </script>
 
 <style scoped>
 .activeCol {
-    color: #E60000;
+  color: #e60000;
 }
 </style>
