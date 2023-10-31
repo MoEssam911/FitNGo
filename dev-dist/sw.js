a561854750a67e7c869a0ad2b62035bdf031c4e2
mohamedElshimi <66941317+mohamedElshimi@users.noreply.github.com>
2023-10-31 13:50:56 +0200

    updated project

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k1b0ap06q8o"
+    "revision": "0.bjsvpnfrtlg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
